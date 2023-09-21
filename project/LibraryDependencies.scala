f50157f0b185c89a25f290d69fe51f838c6f56bb
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-09-21 15:27:35 +0100

    APIS-6412 - Bump some versions

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -4,13 +4,13 @@ object LibraryDependencies {
   def apply() = compileDependencies ++ testDependencies
 
   lazy val compileDependencies = Seq(
-    "uk.gov.hmrc"       %% "api-platform-application-domain" % "0.17.3"
+    "uk.gov.hmrc"       %% "api-platform-application-domain" % "0.18.0"
   )
 
   lazy val testDependencies = Seq(
-    "org.scalactic"       %% "scalactic"               % "3.2.14" % "test",
-    "org.scalatest"       %% "scalatest"               % "3.2.14" % "test",
-    "com.vladsch.flexmark" % "flexmark-all"            % "0.62.2" % "test",
-    "org.mockito"         %% "mockito-scala-scalatest" % "1.7.1"  % "test"
+    "org.scalactic"       %% "scalactic"               % "3.2.14"  % "test",
+    "org.scalatest"       %% "scalatest"               % "3.2.14"  % "test",
+    "com.vladsch.flexmark" % "flexmark-all"            % "0.62.2"  % "test",
+    "org.mockito"         %% "mockito-scala-scalatest" % "1.17.22" % "test"
   )
 }
