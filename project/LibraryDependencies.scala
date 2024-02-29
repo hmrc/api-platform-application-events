7bda237e6aa6f2eb0c644fa14fcc384b3321f4c7
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2024-02-29 15:44:28 +0000

    APIS-6775 Upgrade application domain library version

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object LibraryDependencies {
-  lazy val commonDomainVersion      = "0.12.0"
+  lazy val commonDomainVersion      = "0.13.0"
   lazy val applicationDomainVersion = "0.37.0"
 
   def apply() = compileDependencies ++ testDependencies
