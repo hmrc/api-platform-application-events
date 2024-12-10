150a4b20add4b70e0876270cd34915c56543101e
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2024-12-10 15:35:13 +0000

    APIS-7388 PR bot actions

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -12,11 +12,10 @@ object LibraryDependencies {
   )
 
   lazy val testDependencies = Seq(
-    "com.vladsch.flexmark"  % "flexmark-all"                    % "0.62.2",
-    "org.mockito"          %% "mockito-scala-scalatest"         % "1.17.29",
-    "org.scalactic"        %% "scalactic"                       % "3.2.17",
-    "org.scalatest"        %% "scalatest"                       % "3.2.17",
-    "uk.gov.hmrc"          %% "api-platform-common-domain-fixtures" % commonDomainVersion,
+    "com.vladsch.flexmark"  % "flexmark-all"                             % "0.62.2",
+    "org.mockito"          %% "mockito-scala-scalatest"                  % "1.17.29",
+    "org.scalactic"        %% "scalactic"                                % "3.2.17",
+    "org.scalatest"        %% "scalatest"                                % "3.2.17",
     "uk.gov.hmrc"          %% "api-platform-application-domain-fixtures" % applicationDomainVersion
   ).map(_ % "test")
 }
