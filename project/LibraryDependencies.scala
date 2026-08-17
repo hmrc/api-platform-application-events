a16485434d49dca45c6394bb16880c1ab59e6665
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2026-08-17 10:27:44 +0100

    API-9383-3 - Sort provided out (#106)
    * API-provided - POC to show breaking bonds of libs
    
    * API-9383-3 - Put provided in cleaner place
    
    * API-9383 - Fixing app domain lib
    
    ---------
    
    Co-authored-by: peteslater-ee <28756214+peteslater-ee@users.noreply.github.com>

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -2,24 +2,27 @@ import sbt._
 
 object LibraryDependencies {
 
-  val applicationDomainVersion = "1.5.0"
+  val commonDomainVersion = "1.4.0"
+  val applicationDomainVersion = "1.6.0"
 
-  def apply(scalaVersion: String) = compileDependencies(scalaVersion) ++ testDependencies(scalaVersion)
+  // Cut down and simplified from other libraries as this provides no fixtures
+  
+  def apply = 
+    compileDependencies ++
+    fixturesDependencies.map(_ % "test") ++ 
+    testDependencies
 
-  def compileDependencies(scalaVersion: String) = Seq(
-    "uk.gov.hmrc"       %% "api-platform-application-domain" % applicationDomainVersion
+
+  private def compileDependencies = Seq(
+    "uk.gov.hmrc"       %% "api-platform-common-domain" % commonDomainVersion % "provided",
+    "uk.gov.hmrc"       %% "api-platform-application-domain" % applicationDomainVersion % "provided"
   )
 
-  def testDependencies(scalaVersion: String) = (Seq(
-    "uk.gov.hmrc"          %% "api-platform-application-domain-fixtures" % applicationDomainVersion,
-    "com.vladsch.flexmark"  % "flexmark-all"                             % "0.62.2",
-    "org.scalatest"        %% "scalatest"                                % "3.2.19"
-    ) ++ (
-      CrossVersion.partialVersion(scalaVersion) match {
-        case Some((2,_)) => Seq("org.mockito" %% "mockito-scala-scalatest" % "2.2.1")
-        case _           => Seq("org.scalatestplus" %% "mockito-5-18" % "3.2.19.0")
-      }
-  )).map(_ % "test")
+  private def fixturesDependencies = Seq(
+    "uk.gov.hmrc"             %% "api-platform-common-domain-fixtures" % commonDomainVersion,
+    "uk.gov.hmrc"             %% "api-platform-application-domain-fixtures" % applicationDomainVersion
+  )
+
+  private def testDependencies = Seq.empty[ModuleID]
+    .map(_ % "test")
 }
-
-
